9087b98e5ba210f81a21faf42e98590797e260e3
nserp2023 <nserp2023@gmail.com>
2026-03-23 00:00:37 +0530

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,3 @@
+    const supabaseUrl = "https://gqxczzijntbvtlmmzppt.supabase.co";
+    const supabaseKey = "sb_publishable_kmh1sok1CWBSBW0kvdla7w_T7kDioRs";
+    window.sbcc = supabase.createClient(supabaseUrl, supabaseKey);
